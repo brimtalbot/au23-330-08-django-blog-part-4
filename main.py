7c18156f6e66922d335605ded8fbe1f5c3ec6d0c
Brianna <bmt22@uw.edu>
2023-12-18 17:35:04 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import json
+
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+
+    response = requests.get('https://api.github.com/users/{}/events'.format(username))
+    events = json.loads(response.content)
+
+    print(events[0]['created_at'])
